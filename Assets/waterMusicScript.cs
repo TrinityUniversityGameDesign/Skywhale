0bfaf808f7c1c836e320a4650c161ced53b1d5a8
addisn <addison.freeman@trinity.edu>
2014-05-03 15:58:27 -0500

    skywhale prefab music script

--- Assets/waterMusicScript.cs
+++ Assets/waterMusicScript.cs
@@ -3,18 +3,38 @@ using System.Collections;
 
 public class waterMusicScript : MonoBehaviour {
 
-	public AudioSource BlueDanube;
-	public AudioSource underwaterSound;
+	AudioSource[] sounds;
+	AudioSource musicSource;
+	AudioSource underwaterSource;
+	public AudioClip BlueDanube;
+	public AudioClip underwaterSound;
+	bool underwaterFlag;
 	// Use this for initialization
 	void Start () {
-		//audio.clip = BlueDanube;
-		//audio.Play ();
+		if (this.transform.position.y >= 1) {
+			underwaterFlag = false;
+		} else {
+			underwaterFlag = true;
+		}
+		sounds = GetComponents<AudioSource> ();
+		musicSource = sounds [0];
+		musicSource.clip = BlueDanube;
+		musicSource.Play ();
+		underwaterSource = sounds [1];
+		underwaterSource.clip = underwaterSound;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (this.transform.position.y < 1) {
-			//audio.volume = 0.2;
+
+		if ((this.transform.position.y < 1) && !underwaterFlag) {
+			musicSource.volume = 0.2f;
+			underwaterSource.Play ();
+			underwaterFlag = true;
+		} else if ((this.transform.position.y >= 1) && underwaterFlag) {
+			musicSource.volume = 1f;
+			underwaterSource.Stop ();
+			underwaterFlag = false;
 		}
 	}
 }

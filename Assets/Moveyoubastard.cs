6af07cdfb740c62ae19186230363c5664b115066
kingofferrets <cfisher@trinity.edu>
2014-05-03 11:38:19 -0500

    Beware the moving whale

---
+++ Assets/Moveyoubastard.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moveyoubastard : MonoBehaviour {
+	private Transform pos;
+	// Use this for initialization
+	void Start () {
+		pos = this.gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetButton ("Fire1")) {
+			this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x,this.gameObject.transform.position.y,this.gameObject.transform.position.z-0.1f);
+		}
+		pos.RotateAround(Vector3.zero,Vector3.up,Input.GetAxis("Horizontal"));
+	}
+}

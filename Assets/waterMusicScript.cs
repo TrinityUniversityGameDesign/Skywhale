ea5e9f6beefafcdf5b0577c680375cd7f1e649c6
addisn <addison.freeman@trinity.edu>
2014-05-03 15:14:53 -0500

    underwater sound

---
+++ Assets/waterMusicScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class waterMusicScript : MonoBehaviour {
+
+	public AudioSource BlueDanube;
+	public AudioSource underwaterSound;
+	// Use this for initialization
+	void Start () {
+		audio.clip = BlueDanube;
+		audio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (this.transform.position.y < 1) {
+		audio.
+	}
+
+	}
+}

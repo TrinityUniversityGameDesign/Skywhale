adcd42431e0fed8ce7499f2ba69ce34fdf7d6bfc
kingofferrets <cfisher@trinity.edu>
2014-05-03 14:56:54 -0500

    Merge branch 'master' of https://github.com/TrinityUniversityGameDesign/Skywhale
    Conflicts:
    	Assets/Prefabs/Skywhale.prefab

---
+++ Assets/KeepAngleConstant.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepAngleConstant : MonoBehaviour {
+	public Transform whale;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.rotation = whale.rotation;
+	}
+}

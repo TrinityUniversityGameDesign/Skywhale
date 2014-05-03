7e7be24e95938540df2afcb0a8e39a4786999ca5
addisn <addison.freeman@trinity.edu>
2014-05-03 18:16:39 -0500

    hold g to enable gravity

--- Assets/Scripts/Moveyoubastard.cs
+++ Assets/Scripts/Moveyoubastard.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class Moveyoubastard : MonoBehaviour {
 	private Transform pos;
 	public float speed;
-	public bool gravity = false;
 	// Use this for initialization
 	void Start () {
 		FadeIn.LoadLevel(1 ,0,5,Color.black);
@@ -21,19 +20,17 @@ public class Moveyoubastard : MonoBehaviour {
 		pos.RotateAround(pos.TransformPoint(Vector3.zero),pos.TransformDirection(Vector3.right),Input.GetAxis("Vertical"));
 		pos.RotateAround(pos.TransformPoint(Vector3.zero),pos.TransformDirection(Vector3.forward),Input.GetAxis("Roll"));
 
-		if (Input.GetButtonDown ("Fire3") && !gravity) {
+		if (Input.GetButtonDown ("Fire3")) {
 			this.gameObject.rigidbody.drag = .001f;
 			this.gameObject.rigidbody.angularDrag = .001f;
 			this.gameObject.rigidbody.useGravity = true;
 			this.gameObject.rigidbody.mass = 100f;
-			gravity = true;
 		}
-		if (Input.GetButtonDown ("Fire3") && gravity) {
+		if (Input.GetButtonUp ("Fire3")) {
 			this.gameObject.rigidbody.drag = 100f;
 			this.gameObject.rigidbody.angularDrag = 100f;
 			this.gameObject.rigidbody.useGravity = false;
 			this.gameObject.rigidbody.mass = 1f;
-			gravity = false;
 		}
 	}
 }

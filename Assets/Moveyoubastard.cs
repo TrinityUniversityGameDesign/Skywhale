213eb7d76ea2e89a041abc35a487aba0f7e9c16a
kingofferrets <cfisher@trinity.edu>
2014-05-03 11:50:17 -0500

    Beware the better-moving whale

--- Assets/Moveyoubastard.cs
+++ Assets/Moveyoubastard.cs
@@ -11,8 +11,9 @@ public class Moveyoubastard : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (Input.GetButton ("Fire1")) {
-			this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x,this.gameObject.transform.position.y,this.gameObject.transform.position.z-0.1f);
+			pos.Translate(0,0,-0.1f);
 		}
-		pos.RotateAround(Vector3.zero,Vector3.up,Input.GetAxis("Horizontal"));
+		pos.RotateAround(pos.TransformPoint(Vector3.zero),Vector3.up,Input.GetAxis("Horizontal"));
+		pos.RotateAround(pos.TransformPoint(Vector3.zero),Vector3.forward,Input.GetAxis("Vertical"));
 	}
 }

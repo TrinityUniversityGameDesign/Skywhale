d90c9c0600b75827e720cb1401ef86cfca171fc4
bpauls <bpauls@trinity.edu>
2014-05-03 17:19:10 -0500

    Blowhole

---
+++ Assets/Blow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blow : MonoBehaviour {
+	private ParticleEmitter hole;
+
+
+	// Use this for initialization
+	void Start () {
+		hole = this.particleEmitter;
+		hole.emit = false;
+	}
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Blowhole")) {
+			hole.emit = true;
+				}
+		else {
+			hole.ClearParticles();
+			hole.emit = false;
+		}
+
+	}
+}

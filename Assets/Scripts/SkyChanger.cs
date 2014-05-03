9d964ac700bbaa3a5d3dbf84592366cafcfb618c
vivianha <vha@trinity.edu>
2014-05-03 15:11:46 -0500

    Sky changer and water

---
+++ Assets/Scripts/SkyChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyChanger : MonoBehaviour {
+	public Material sky;
+	public Material water;
+	public GameObject whale;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.gameObject.transform.position.y > whale.transform.position.y){
+			RenderSettings.skybox = water;
+		}
+		else{
+			RenderSettings.skybox = sky;
+		}
+	}
+}

b7f5f4fef199e85c8f33d31d63c6ae38f7210b5c
bpauls <bpauls@trinity.edu>
2014-05-03 16:11:10 -0500

    Bubble

---
+++ Assets/underwatereffect.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class underwatereffect : MonoBehaviour {
+
+	public float waterLevel;
+	
+	public Transform waterPlane;    //  Testing
+	
+	private bool isUnderwater;
+	
+	private Color normalColor;
+	
+	public Color underwaterColor;
+
+
+
+	
+	
+	// Use this for initialization
+	
+	void Start () {
+		
+		normalColor = new Color (0.5f, 0.5f, 0.5f, 0.5f);
+		
+	//	underwaterColor = new Color (0.22f, 0.65f, 0.77f, 0.5f);
+
+		waterPlane = GameObject.FindGameObjectWithTag ("Water").transform;
+		waterLevel = waterPlane.transform.position.y;
+
+	}
+	
+	
+	
+	// Update is called once per frame
+	
+	void Update () {
+		
+		if ((transform.position.y < waterLevel) != isUnderwater) {
+			
+			isUnderwater = transform.position.y < waterLevel;
+			
+			if (isUnderwater) SetUnderwater ();
+			
+			if (!isUnderwater) SetNormal ();
+			
+		}
+		
+	}
+	
+	
+	
+	void SetNormal () {
+		
+		RenderSettings.fogColor = normalColor;
+		
+		RenderSettings.fogDensity = 0.01f;
+		
+		
+		
+		//  Testing
+		
+	//	waterPlane.localScale = new Vector3 (waterPlane.localScale.x, 1.0f, waterPlane.localScale.z);
+		
+	}
+	
+	
+	
+	void SetUnderwater () {
+		
+		RenderSettings.fogColor = underwaterColor;
+		
+		RenderSettings.fogDensity = 200f;
+		
+		
+		
+		//  Testing
+		
+	//	waterPlane.localScale = new Vector3 (waterPlane.localScale.x, -1.0f, waterPlane.localScale.z);
+		
+	}
+	
+}

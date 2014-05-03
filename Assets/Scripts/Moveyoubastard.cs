147dc5cb6dc1314aa2d01a84314d3010728e6d6e
addisn <addison.freeman@trinity.edu>
2014-05-03 18:14:02 -0500

    new whale prefab! everything should work!

--- Assets/Scripts/Moveyoubastard.cs
+++ Assets/Scripts/Moveyoubastard.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Moveyoubastard : MonoBehaviour {
 	private Transform pos;
 	public float speed;
-	bool gravFlag = true;
+	public bool gravity = false;
 	// Use this for initialization
 	void Start () {
 		FadeIn.LoadLevel(1 ,0,5,Color.black);
@@ -17,23 +17,24 @@ public class Moveyoubastard : MonoBehaviour {
 		if (Input.GetButton ("Fire1")) {
 			pos.Translate(0,0,-speed);
 		}
+		pos.RotateAround(pos.TransformPoint(Vector3.zero),pos.TransformDirection(Vector3.up),Input.GetAxis("Horizontal"));
+		pos.RotateAround(pos.TransformPoint(Vector3.zero),pos.TransformDirection(Vector3.right),Input.GetAxis("Vertical"));
+		pos.RotateAround(pos.TransformPoint(Vector3.zero),pos.TransformDirection(Vector3.forward),Input.GetAxis("Roll"));
 
-		if (Input.GetButtonDown ("Fire3") && gravFlag) {
-			gravFlag = false;
+		if (Input.GetButtonDown ("Fire3") && !gravity) {
 			this.gameObject.rigidbody.drag = .001f;
 			this.gameObject.rigidbody.angularDrag = .001f;
 			this.gameObject.rigidbody.useGravity = true;
 			this.gameObject.rigidbody.mass = 100f;
+			gravity = true;
 		}
-		if (Input.GetButtonUp ("Fire3") || !gravFlag) {
+		if (Input.GetButtonDown ("Fire3") && gravity) {
 			this.gameObject.rigidbody.drag = 100f;
 			this.gameObject.rigidbody.angularDrag = 100f;
 			this.gameObject.rigidbody.useGravity = false;
 			this.gameObject.rigidbody.mass = 1f;
-			gravFlag = true;
+			gravity = false;
 		}
-		pos.RotateAround(pos.TransformPoint(Vector3.zero),pos.TransformDirection(Vector3.up),Input.GetAxis("Horizontal"));
-		pos.RotateAround(pos.TransformPoint(Vector3.zero),pos.TransformDirection(Vector3.right),Input.GetAxis("Vertical"));
-		pos.RotateAround(pos.TransformPoint(Vector3.zero),pos.TransformDirection(Vector3.forward),Input.GetAxis("Roll"));
 	}
 }
+

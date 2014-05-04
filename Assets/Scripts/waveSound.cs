7dff01fa92d94c39d332662b17a71ced8fbc0e10
addisn <addison.freeman@trinity.edu>
2014-05-03 19:10:02 -0500

    added tag, adding sounds

---
+++ Assets/Scripts/waveSound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class waveSound : MonoBehaviour {
+	public AudioClip waves;
+	public GameObject skywhale;
+
+	// Use this for initialization
+	void Start () {
+		audio.clip = waves;
+		audio.Play ();
+		//skywhale = GameObject.FindGameObjectWithTag ("Whale");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((skywhale.transform.position.y > 15) && !audio.mute){
+			audio.mute = true;
+		} else if (skywhale.transform.position.y <= 15 && audio.mute) {
+			audio.mute = false;
+		}
+	}
+}
